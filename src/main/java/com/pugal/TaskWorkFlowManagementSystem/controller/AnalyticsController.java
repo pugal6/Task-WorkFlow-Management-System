13edcf711b3be353889eed8a9faaf28f04c16821
pugal <upugal03@gmail.com>
2026-03-24 17:28:12 +0530

    Implement analytics REST APIs

---
+++ src/main/java/com/pugal/TaskWorkFlowManagementSystem/controller/AnalyticsController.java
@@ -0,0 +1,36 @@
+package com.pugal.TaskWorkFlowManagementSystem.controller;
+import com.pugal.TaskWorkFlowManagementSystem.enums.TaskStatus;
+import com.pugal.TaskWorkFlowManagementSystem.service.AnalyticsService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/analytics")
+public class AnalyticsController {
+
+    private final AnalyticsService analyticsService;
+
+    public AnalyticsController(AnalyticsService analyticsService) {
+        this.analyticsService = analyticsService;
+    }
+
+    @GetMapping("/tasks/count-by-status/{status}")
+    public ResponseEntity<Long> getTaskCountByStatus(@PathVariable TaskStatus status) {
+
+        long count = analyticsService.getTaskCountByStatus(status);
+        return ResponseEntity.ok(count);
+    }
+
+    @GetMapping("/tasks/completed-on/{date}")
+    public ResponseEntity<Long> getCompletedTasksOnDate(
+            @PathVariable
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date) {
+
+        long count = analyticsService.getCompletedTasksOnDate(date);
+        return ResponseEntity.ok(count);
+    }
+}
